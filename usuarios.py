bdb2fc8188ca9fcb343d720ee10fccb516f73e03
Miguel Á. Pamos <mpamure779@g.educaand.es>
2024-04-14 22:24:40 +0200

    Añadido usuarios.py

---
+++ usuarios.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import numpy as np
+import random
+
+data = {
+
+
+    'Nombre': ['Juan', 'Maria', 'Pedro', 'Laura'],
+    'Edad': [25, 30, 28, 35],
+    'Ciudad': ['Madrid', 'Barcelona', 'Valencia', 'Sevilla']
+}
+
+df = pd.DataFrame(data)
+print(df)
+
+
